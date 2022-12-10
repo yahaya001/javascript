68353d5f77fbbff288e7f5b3d6f525ed2a274f1d
yahaya001 <yahayagusau6360>
2022-12-10 23:09:51 +0000

    _

---
+++ recursion.js
@@ -0,0 +1,19 @@
+let num = 0;
+function recursive(){
+    console.log("hello aliens", num)
+    num++;
+    //recursive()
+}
+//recursive();
+
+//TRY SOLVE FACTORIAL USING LOOP
+
+// let fact = 5;
+// for(let i =5, j=fact; i>0; i--, j--){
+//     console.log(i*j)
+// }
+function fact(factnum){
+    fact()
+}
+fact(6)
+//console.log(fact(5))
